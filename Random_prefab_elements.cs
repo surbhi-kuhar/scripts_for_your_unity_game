b72da86fe80e83bc3b5958565ebfaf3453fea605
surbhi-kuhar <104093804+surbhi-kuhar@users.noreply.github.com>
2023-02-04 18:09:25 +0530

    Generate Random elements at Random intervals

--- Random_prefab_elements.cs
+++ Random_prefab_elements.cs
@@ -7,23 +7,29 @@ public class SpawManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnRangeZ = 20;
+    
+    private float startDelay = 2f;
+    private float spawnInterval = 1.5f;
    
     void Start()
     {
+    
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval); 
         
     }
 
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            
-            int animalIndex = Random.Range(0,animalPrefabs.Length);
-            Vector3 spawPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
-            Instantiate(animalPrefabs[animalIndex], spawPos, animalPrefabs[animalIndex].transform.rotation);
-            
-        }
         
+        
+    }
+    
+    void SpawnRandomAnimal() {
+         
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
+        Instantiate(animalPrefabs[animalIndex], spawPos, animalPrefabs[animalIndex].transform.rotation);
+
     }
 }
